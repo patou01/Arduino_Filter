f9b394acaca69ad106d29d6a3b29867526eac746
root <pbobbink@gmail.com>
2018-04-30 09:42:12 +0200

    small fix

--- examples/Filter_demo/Filter.cpp
+++ examples/Filter_demo/Filter.cpp
@@ -22,7 +22,7 @@ Filter::Filter(int loFreq, int hiFreq, int dt) // set hiFreq = 0 and loFreq > 0 
   data = 0;
   output = 0;
   loPassAlpha = 2*PI*dt*1e-3*hiFreq/(2*PI*dt*1e-3*hiFreq + 1);
-  hiPassAlpha = 1/(2*PI*dt*1e-3*loFreq+1);
+  hiPassAlpha = 1/(2*PI*dt*1e-3*loFreq + 1);
 }
 
 
@@ -60,8 +60,7 @@ void Filter::addData(float data_)
   output = loPassAlpha*data + (1-loPassAlpha)*output;
 
   // high pass
-  if(hiPassAlpha > 0.001)
-    output = hiPassAlpha*output + hiPassAlpha*(data - prevData);
+  output = hiPassAlpha*output + hiPassAlpha*(data - prevData);
 }
 
 float Filter::getLoPassAlpha()
